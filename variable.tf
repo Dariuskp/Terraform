f6733b045f5dc50d94467c2fc43f161993c21b6f
Dariuskp <89350823+Dariuskp@users.noreply.github.com>
2021-09-29 13:27:37 -0500

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,42 @@
+variable "vpc-cidr" {
+    default = "10.0.0.0/16"
+    description = "VPC CIDR Block"
+    type = string
+}
+
+variable "public-subnet-1-cidr" {
+    default = "10.0.0.0/24"
+    description = "Public Subent 1 CIDR Block"
+    type = string
+}
+
+variable "public-subnet-2-cidr" {
+    default = "10.0.1.0/24"
+    description = "Public Subent 2 CIDR Block"
+    type = string
+}
+
+variable "private-subnet-1-cidr" {
+    default = "10.0.2.0/24"
+    description = "Private Subent 1 CIDR Block"
+    type = string
+}
+
+variable "private-subnet-2-cidr" {
+    default = "10.0.3.0/24"
+    description = "Private Subent 2 CIDR Block"
+    type = string
+}
+
+variable "private-subnet-3-cidr" {
+    default = "10.0.4.0/24"
+    description = "Private Subent 3 CIDR Block"
+    type = string
+}
+
+variable "private-subnet-4-cidr" {
+    default = "10.0.5.0/24"
+    description = "Private Subent 4 CIDR Block"
+    type = string
+}
+
